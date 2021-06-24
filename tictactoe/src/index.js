61f63087018a6736e19447ceb3623e4a36896b28
Nicolas Van Damme <nicolas.vandamme@student.hogent.be>
2021-06-24 16:04:17 +0200

    Fixed scope of constant var

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -271,19 +271,20 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const LINES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let i = 0; i < LINES.length; i++) {
+    const [a, b, c] = LINES[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
