8364088a7520e1949acf56bd95644c65f295c4e1
Nicolas Van Damme <nicolas.vandamme@student.hogent.be>
2021-06-24 11:55:26 +0200

    Added ESLint

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
